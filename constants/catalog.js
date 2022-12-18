e4d358e2dbcc2a0db2aba3b16b1bce4caee583e3
MalyshevaElena <ger.malysheva@yandex.ru>
2022-12-18 18:21:41 +0700

    Initial Commit

---
+++ constants/catalog.js
@@ -0,0 +1,51 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Le Grand NoiLe Grand Noir" GSM',
+        img: 'https://i.ibb.co/YyPT7jn/0-0-prod-desktop.jpg',
+        price: 1250,
+    },
+    {
+        id: 'el2',
+        name: 'Vignerons du Pallet, Muscadet Sevre et Maine sur Lie',
+        img: 'https://i.ibb.co/WkfxGtj/0-0-prod-desktop-2.jpg',
+        price: 995,
+    },
+    {
+        id: 'el3',
+        name: 'Vignerons Catalans, "Cellier des Vicomtes" Rouge',
+        img: 'https://i.ibb.co/jfS4f0P/0-0-orig.jpg',
+        price: 635,
+    },
+    {
+        id: 'el4',
+        name: '"Haut Pericou", Bordeaux',
+        img: 'https://i.ibb.co/DLv8mrj/0-0-orig-2.jpg',
+        price: 1006,
+    },
+    {
+        id: 'el5',
+        name: 'Malbec, Luigi Bosca',
+        img: 'https://i.ibb.co/4gHrzDb/c8a9d0580e74ddd82a911ce8182f90ee-png-x430.png',
+        price: 1690,
+    },
+    {
+        id: 'el6',
+        name: 'Rose Art Collection, Gurpegui',
+        img: 'https://i.ibb.co/2y8hnns/d1c72356bd9da0eb96547e54f7ee6be9-jpg-x430.png',
+        price: 890,
+    },
+    {
+        id: 'el7',
+        name: 'Tenuta Regaleali Bianco, Conte Tasca dAlmerita',
+        img: 'https://i.ibb.co/T8SVWVm/780f680b439e1b4aa411bd4bd1ebd458-png-x430.png',
+        price: 1493,
+    },
+    {
+        id: 'el8',
+        name: 'Chianti Riserva, Melini',
+        img: 'https://i.ibb.co/WWz629S/a35853cb9dec5565dffbe5dac4c0e381-jpg-x430.jpg',
+        price: 1118,
+    },
+];
+
